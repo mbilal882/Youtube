b6d9b5dc3b3a3f2839972374874b19ab7071120b
Muhammad Bilal <mbilal78692110@gmail.com>
2024-08-11 16:07:12 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Youtube
+Created with CodeSandbox
+Try the demo now:- 
+                https://codesandbox.io/s/github/bilal882s/Youtube
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <iframe
+        width="1366"
+        height="768"
+        src="https://www.youtube.com/embed/5oJGsw2lECo"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
